2fa05387e0807a7a0dbf2ef1bcb61687f9d1f8ac
rebeccacaroline <rebeccaholzschuh@gmail.com>
2016-02-04 14:27:19 -0800

    add new relic agent

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('newrelic');
+
 require('dotenv').load();
 var express = require('express');
 var app = express();
---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['MyBackboneLibrary'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'e0c7c805dc6d8a19ba42bcf0ef1de749c5fb6bdc',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
